2a88b4eb657d80bcc29dc7ebca4c84838a895373
Christian Saunders <noname45688@gmail.com>
2020-07-11 04:55:29 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,1 +1,7 @@
+import string
 
+# NLP Sentiment/Text Analysis
+
+# Create text variable and clean itp
+text = open('read.txt', encoding='utf-8').read().lower().translate(str.maketrans('','',string.punctuation))
+print(text)
