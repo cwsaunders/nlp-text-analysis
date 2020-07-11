ace991318b9f97765ebd2d57b4f2c1c947f25bd7
Christian Saunders <noname45688@gmail.com>
2020-07-11 05:36:28 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,4 +4,16 @@ import string
 
 # Create text variable and clean itp
 text = open('read.txt', encoding='utf-8').read().lower().translate(str.maketrans('','',string.punctuation))
-print(text)
+
+tokenized_words = text.split()
+
+# Take specified stop words from .txt file
+stop_words = open('stop-words.txt', encoding='utf-8').read().lower().split()
+
+final_words = []
+
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print(final_words)
